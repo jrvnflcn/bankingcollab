9747ad63800e3aedf7575c0fbe2580d3e9e57c9b
Jeane Feliciano <jrvnflcn@gmail.com>
2024-10-29 22:31:23 +0800

    adjusted background for login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,15 @@
 import logo from './assets/Northland Bank Logo.png';
 import './Login.css';
+import background from './assets/Background.mp4';
 
 
 
 const Login = () => {
     return ( <div className="login-container">
         
-        Hello
+        <video autoplay="autoplay" muted loop id="myVideo"
+         source src={background} type="video/mp4">
+        </video>
     
     
     </div> );
